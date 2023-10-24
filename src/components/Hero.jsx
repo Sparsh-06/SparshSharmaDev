1997c4ac9f2628211e8b87f32ef350131b15dbd9
Sparsh Sharma <sparshsharma8700@gmail.com>
2023-10-24 19:02:28 +0530

    Profile Uodate

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Hero = () => {
   return (
-
     <div className="grid grid-cols-1 h-full md:grid-cols-2 lg:h-190 justify-center items-center container px-16">
       <div className="text-center md:text-left text-white">
         <span className="text-lg md:text-xl after:content-['👍']">Hello</span>
         <h2 className="text-5xl my-2 text-[#01d293]">Welcome to my space</h2>
         <p className="text-lg md:text-lg lg:text-xl my-4">Software Developer</p>
         <p className="text-[#808dad]">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
-          possimus necessitatibus mollitia accusantium illo voluptatem
-          voluptatibus sint expedita error eaque. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Minus amet provident est asperiores fuga
-          repellendus quas iure vitae id consequuntur.
+          Passionate and innovative Full Stack Website and Python
+          programmer/developer with experience of 3+ years with the ability to
+          establish and maintain excellent communication and relationships with
+          clients. Proficient in various platforms, languages, and embedded
+          systems. A creative thinker, adept in software development and working
+          with various data structures.
         </p>
       </div>
       <img
